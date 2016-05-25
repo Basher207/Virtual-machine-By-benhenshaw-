6ea45ec5f6bcc435fc173b07cdaae617c8557978
Benedict Henshaw <benedicthenshaw@gmail.com>
2016-05-25 15:52:43 +0100

    Fix build script

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-gcc *.c -IC:/MinGW/include -LC:/MinGW/lib -Wall -pedantic -lmingw32 -mwindows -lSDL2main -lSDL2 -o build/chip8.exe; build/chip8 build/games/MAZE
+gcc src/*.c -IC:/MinGW/include -LC:/MinGW/lib -Wall -pedantic -lmingw32 -mwindows -lSDL2main -lSDL2 -o build/chip8.exe; build/chip8 build/games/MAZE
